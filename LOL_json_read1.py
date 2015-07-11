3c8fb6b088513a0eb179feaf57e97b856e7937da
Thomas Burg <t1@thomas-burg.de>
2015-07-11 20:39:55 +0200

    fixed bug in NA processing now truly region-flexible

--- LOL_json_read1.py
+++ LOL_json_read1.py
@@ -1,7 +1,7 @@
 __author__ = 'Thomas'
 
-NAME="LOLPyth"
-VERSION="1.01"
+NAME = "LOLPyth"
+VERSION = "1.01"
 
 import urllib2 as urllib
 import json
@@ -48,186 +48,209 @@ u'stats':
 '''
 
 # LOL API credentials
-api_keys={
+api_keys = {
   "na": "268306a5-a526-4535-800c-623be8313f74",
-  "euw":"71be2bcb-e5de-4784-bede-3573498311f2"
+  "euw": "71be2bcb-e5de-4784-bede-3573498311f2"
 }
 
-loglevel=99 # global var !
+loglevel = 99  # global var !
 
 http_method = "GET"
 
-http_handler  = urllib.HTTPHandler(debuglevel=loglevel)
+http_handler = urllib.HTTPHandler(debuglevel=loglevel)
+
 
 def Linefeed():
-  print "----------------------------------------------------------------"
+  print
+  "----------------------------------------------------------------"
+
 
 def get_api_key(region):
-  for key,value in api_keys.items():
-    if key==region:
-      if loglevel>50:
-        print "API key for region",region,"is ",value
-      return "api_key="+value
+  for key, value in api_keys.items():
+    if key == region:
+      if loglevel > 50:
+        print
+        "API key for region", region, "is ", value
+      return "api_key=" + value
   return "???"
 
-def getJSONResponse(context,url):
-  if region=="na":
-    url="https://na.api.pvp.net/"+url
-  elif region=="euw":
-    url="https://euw.api.pvp.net/"+url
 
-  if loglevel>40:
-    print context+": URL is "+url
-  httpresponse=urllib.urlopen(url)
-  jsonresponse=httpresponse.read()
-  j1=json.loads(jsonresponse)
-  if loglevel>80:
-    print context+": json resp is "+str(j1)
+def getJSONResponse(region,context, url_end):
+  if region == "na":
+    full_url = "https://na.api.pvp.net/" + url_end
+  elif region == "euw":
+    full_url = "https://euw.api.pvp.net/" + url_end
+  elif region=="global":
+    full_url= "https://global.api.pvp.net/api/lol/static-data" + url_end
+  else:
+    print"unknown region"
+    sys.exit()
+
+  if loglevel > 40:
+    print context + ": URL is " + full_url
+  httpresponse = urllib.urlopen(full_url)
+  jsonresponse = httpresponse.read()
+  j1 = json.loads(jsonresponse)
+  if loglevel > 80:
+    print
+    context + ": json resp is " + str(j1)
   return j1
 
-def get_champion_name(id,region):
-  url="api/lol/static-data/"+region+"/v1.2/champion/"
-  url=url+str(id)
-  url=url+"?"+get_api_key(region)
-  resp=getJSONResponse("get_champion_name`",url)
+
+def get_champion_name(id, region):
+  url_end = "/"+region + "/v1.2/champion/"
+  url_end = url_end + str(id)
+  url_end = url_end + "?" + get_api_key(region)
+  resp = getJSONResponse("global", "get_champion_name", url_end)
   return resp["name"]
 
-def getSummonerId(name,region):
-  url="api/lol/"+region+\
-      "/v1.4/summoner/by-name/"+name+\
-      "?"+get_api_key(region)
-  resp=getJSONResponse("getSummonerId",url)
-  j2=resp[name]
-  id=j2["id"]
+
+def getSummonerId(name, region):
+  url_end = "api/lol/" + region + \
+        "/v1.4/summoner/by-name/" + name + \
+        "?" + get_api_key(region)
+  resp = getJSONResponse(region, "getSummonerId", url_end)
+  j2 = resp[name]
+  id = j2["id"]
   return id
 
-def safeGetStats(stats,value):
+
+def safeGetStats(stats, value):
   try:
-    theResult=stats[value]
+    theResult = stats[value]
   except Exception, e:
-    theResult=0
+    theResult = 0
   return theResult
 
-def printSummonerGameList(id,region, do_csv):
-  url1="api/lol/"+region+"/v1.3/game/by-summoner/"
-  url2="/recent?"+get_api_key(region)
-  url=url1+str(id)+url2
 
-  resp=getJSONResponse("printSummonerGameList",url)
-  gamelist=resp["games"]
-  if loglevel>30:
+def printSummonerGameList(id, region, do_csv):
+  url1 = "api/lol/" + region + "/v1.3/game/by-summoner/"
+  url2 = "/recent?" + get_api_key(region)
+  url = url1 + str(id) + url2
+
+  resp = getJSONResponse(region,"printSummonerGameList", url)
+  gamelist = resp["games"]
+  if loglevel > 30:
     Linefeed()
-    print "+++ gamelist is: <" + str(gamelist)+">"
+    print "+++ gamelist is: <" + str(gamelist) + ">"
 
   if do_csv:
-    print "#\tWhen\tMins\tGameType\tSubType\tMode\tLvl"\
+    print "#\tWhen\tMins\tGameType\tSubType\tMode\tLvl" \
       "\tWon?\tChampion\tDamTC\tGEarn\tGSold\tKMni\tKTur\tKill\tDeth\tAsst"
   else:
-    print " # When                   Mins GameType     "+\
+    print " # When                   Mins GameType     " + \
       "   SubType              Mode      Lvl" \
       " Won?   Champion        DamTC GEarn GSold KMni KTur Kill Deth Asst"
 
   if not do_csv:
-    print " - ------------------------ -- --------     "+\
-      "   -------------------- --------  ---" \
-      " ---- ----------------  ----- ----- ----- ---- ---- ---- ---- ----"
+    print \
+    " - ------------------------ -- --------     " + \
+    "   -------------------- --------  ---" \
+    " ---- ----------------  ----- ----- ----- ---- ---- ---- ---- ----"
 
-  gamenum=10
+  gamenum = 10
 
   for game in gamelist:
-    if loglevel>0:
+    if loglevel > 0:
       Linefeed()
-      print "*** game is <" + str(game)+">"
+      print "*** game is <" + str(game) + ">"
 
     # gather some data
-    championId=game["championId"]
-    championName=get_champion_name(championId,region)
+    championId = game["championId"]
+    championName = get_champion_name(championId, region)
 
     # stats
-    stats=game["stats"]
-    if loglevel==88:
+    stats = game["stats"]
+    if loglevel == 88:
       print stats;
-    minutes=int(stats["timePlayed"])/60
+    minutes = int(stats["timePlayed"]) / 60
 
-    numtime=game["createDate"]/1000
-    gametime=time.ctime(numtime)
+    numtime = game["createDate"] / 1000
+    gametime = time.ctime(numtime)
 
     # physical damage if present
     try:
-      pd=stats["physicalDamageDealtToChampions"]
+      pd = stats["physicalDamageDealtToChampions"]
     except Exception, e:
-      pd="???"
+      pd = "???"
 
     # and print it
     if do_csv:
-      TXT="\""
-      SEP="\t"
+      TXT = "\""
+      SEP = "\t"
     else:
-      TXT=""
-      SEP=" "
+      TXT = ""
+      SEP = " "
     print \
-      string.rjust(str(gamenum),2)+SEP+\
-      TXT+gametime+TXT+SEP+\
-      str(minutes)+SEP+\
-      string.ljust(str(game["gameType"]),15)+SEP+\
-      string.ljust(str(game["subType"]),20)+SEP+\
-      string.ljust(game["gameMode"],10)+SEP+\
-      string.rjust(str(game["level"]),2)+SEP+\
-      string.ljust(str(stats["win"]),6)+SEP+\
-      string.ljust(championName,15)+SEP+\
-      string.rjust(str(pd),5)+SEP+\
-      string.rjust(str(stats["goldEarned"]),5)+SEP+\
-      string.rjust(str(stats["goldSpent"]),5)+SEP+\
-      string.rjust(str(stats["minionsKilled"]),4)+SEP+\
-      string.rjust(str(safeGetStats(stats,"turretsKilled")),4)+SEP+\
-      string.rjust(str(safeGetStats(stats,"championsKilled")),4)+SEP+\
-      string.rjust(str(safeGetStats(stats,"numDeaths")),4)+SEP+\
-      string.rjust(str(safeGetStats(stats,"assists")),4)
+        string.rjust(str(gamenum), 2) + SEP + \
+        TXT + gametime + TXT + SEP + \
+        str(minutes) + SEP + \
+        string.ljust(str(game["gameType"]), 15) + SEP + \
+        string.ljust(str(game["subType"]), 20) + SEP + \
+        string.ljust(game["gameMode"], 10) + SEP + \
+        string.rjust(str(game["level"]), 2) + SEP + \
+        string.ljust(str(stats["win"]), 6) + SEP + \
+        string.ljust(championName, 15) + SEP + \
+        string.rjust(str(pd), 5) + SEP + \
+        string.rjust(str(stats["goldEarned"]), 5) + SEP + \
+        string.rjust(str(stats["goldSpent"]), 5) + SEP + \
+        string.rjust(str(stats["minionsKilled"]), 4) + SEP + \
+        string.rjust(str(safeGetStats(stats, "turretsKilled")), 4) + SEP + \
+        string.rjust(str(safeGetStats(stats, "championsKilled")), 4) + SEP + \
+        string.rjust(str(safeGetStats(stats, "numDeaths")), 4) + SEP + \
+        string.rjust(str(safeGetStats(stats, "assists")), 4)
 
     # increase counter
-    gamenum=gamenum-1
+    gamenum = gamenum - 1
+
 
 def usage():
-  print "usage: " + sys.argv[0] + " <name of summoner> <region> <csv> <debug>"
-  print "   <csv>   - Y or y for Excel readable format"
-  print "           - any other value for human readable format"
-  print "   <debug> - 0 for quiet mode"
-  print "         - any other value for human readable format"
+  print
+  "usage: " + sys.argv[0] + " <name of summoner> <region> <csv> <debug>"
+  print
+  "   <csv>   - Y or y for Excel readable format"
+  print
+  "           - any other value for human readable format"
+  print
+  "   <debug> - 0 for quiet mode"
+  print
+  "         - any other value for human readable format"
+
 
 def dummy_main():
   return
 
+def hello(do_csv):
+  print NAME, " version ", VERSION
+
 if __name__ == '__main__':
   # default summoner_name
-  summoner_name="lgw2015" # "sayna" mtyranus
+  summoner_name = "lgw2015"  # "sayna" mtyranus
 
   # get runtime parameters
   try:
-    summoner_name=sys.argv[1]
-    region=sys.argv[2]
-    do_csv=sys.argv[3]
-    loglevel=int(sys.argv[4])
-  except Exception,e:
+    summoner_name = sys.argv[1]
+    region = sys.argv[2]
+    do_csv = sys.argv[3]
+    loglevel = int(sys.argv[4])
+  except Exception, e:
     usage()
     sys.exit()
 
-  if do_csv=="y" or do_csv=="Y":
-    do_csv=True
+  if do_csv == "y" or do_csv == "Y":
+    do_csv = True
   else:
-    do_csv=False
+    do_csv = False
+
+  # startup msg
+  hello(do_csv)
 
   if not do_csv:
-    print "human readable output, region '"+region+"', summoner '"\
-      +summoner_name+"', debug "+str(loglevel)
+    print "human readable output, region '" + region + "', summoner '" \
+      + summoner_name + "', debug " + str(loglevel)
 
   # convert to id
-  id=getSummonerId(summoner_name,region)
+  id = getSummonerId(summoner_name, region)
 
-  # headline
-  if not do_csv:
-    print NAME, " version ", VERSION
-    print "--- Statistics for "+region+" Summoner , "+summoner_name #+time.ctime(time.now())
-
-  # get stats
-  printSummonerGameList(id,region,do_csv)
-
+ # get stats
+  printSummonerGameList(id, region, do_csv)
